768ddf729c01e4a7a55ad362f7e2e48113ae0b14
Rohit <irohit427@gmail.com>
2021-08-10 19:43:29 +0530

    support for multiple keys

--- src/arraymethods.js
+++ src/arraymethods.js
@@ -21,11 +21,13 @@ class ArrayMethods {
 		return to;
 	}
 	
-	static remove(key, arr) {
-		let index = arr.indexOf(key);
-		if (index > -1) {
-			arr.splice(index, 1);
-		}
+	static remove(keys, arr) {
+		_.each(keys, (key) => {
+			let index = arr.indexOf(key);
+			if (index > -1) {
+				arr.splice(index, 1);
+			}
+		})
 		return arr;
 	}
 }

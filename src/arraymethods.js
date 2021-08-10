5fdfe5e0287a0d5835ce610c45d15ea89856f02a
Rohit <irohit427@gmail.com>
2021-08-10 16:16:12 +0530

    added remove element func

--- src/arraymethods.js
+++ src/arraymethods.js
@@ -20,6 +20,14 @@ class ArrayMethods {
 		});
 		return to;
 	}
+	
+	static remove(key, arr) {
+		let index = arr.indexOf(key);
+		if (index > -1) {
+			arr.splice(index, 1);
+		}
+		return arr;
+	}
 }
 
 module.exports = ArrayMethods;
